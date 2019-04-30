9ad5b4643a5b6682ad8d3dc038c8c0be27006a55
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2019-04-30 22:11:00 +0200

    Added menu and instructions

--- BolzanoWeierstrass.cpp
+++ BolzanoWeierstrass.cpp
@@ -10,35 +10,59 @@ double ele(double b, int e);
 void aproximacioSecant(int iteracions, int decimals);
 void aproximacioBolzano(int iteracions, int decimals);
 double evaluar (double x);
+void menu ();
 
 char numeros[] = {'0','1','2','3','4','5','6','7','8','9'};
 int coeficients [100];
 int exponents [100];
 bool termeIndependent = false;
 int grau = 0;
+double valSecant = 1;
+double valBolzano = 1;
 
 int main () {
 	int l = 100;
 	char expressio [l];
-	cin.getline(expressio, l);
-	processar (expressio, l);
-	cout << "Evaluant una funcio de grau " << grau << endl;
-	aproximacioSecant (10, 50);
-	aproximacioBolzano (10, 50);
-//	for (int i = 0; i<100; i++) {
-//		cout << coeficients[i] << " / ";
-//	}
-//	cout << endl;
-//	for (int i = 0; i<100; i++) {
-//		cout << exponents[i] << " / ";
-//	}
-//	cout << endl;
+	int decisio;
+	int iteracions;
+	int precisioDecimals;
+	bool sortir = false;
+	while (!sortir) {
+		cout << "Introduir l'expressio: ";
+		cin.getline(expressio, l);
+		processar (expressio, l);
+		menu();
+		cin >> decisio;
+		cout << "Numero de iteracions: ";
+		cin >> iteracions;
+		cout << "Quantitat de decimals: ";
+		cin >> precisioDecimals;
+		cin.ignore();
+		cout << "\nEvaluant una funcio de grau " << grau << endl;
+		switch (decisio) {
+			case 1:
+				aproximacioBolzano(iteracions, precisioDecimals);
+				break;
+			case 2:
+				aproximacioSecant(iteracions, precisioDecimals);
+				break;
+			case 3:
+				aproximacioBolzano(iteracions, precisioDecimals);
+				aproximacioSecant(iteracions, precisioDecimals);
+				cout << "La aproximacio de la secant ha estat " << valBolzano/valSecant << " vegades mes eficient" << endl;
+				break;
+			case 0:
+				sortir = true;
+				break;
+		}
+		cout << "\n\n";
+	}
+	return 0;
 }
 
 void processar (char* e, int l) {
 	int ronda=0;
 	for (int i=0; i<l; i++) {
-//		cout << "Hola " << i << endl;
 		bool negative = false;
 		int numero = 0;
 		int exponent = 0;
@@ -47,29 +71,19 @@ void processar (char* e, int l) {
 			while (conte(numeros, e[i-k], 10)) {
 				k++;
 			}
-//			cout << "valor de k: " << k << endl;
 			for (int j=k-1; j>0; j--) {
 				numero += (e[i-j]-48)*ele((double)10, j-1);
-//				cout << "Dins del metode I" << endl;
 			}
-//			cout << numero << endl;
-//			cout << "Fora del metode I" << endl;
 			if (e[i-k] == '-') {
-//				cout << "Dins del metode II" << endl;
 				negative=true;
 			}
-//			cout << "Fora del metode II" << endl;
 			int m=1;
 			while (conte(numeros, e[i+m], 10)) {
-//				cout << "Dins del metode III" << endl;
 				m++;
 			}
-//			cout << "Fora del metode III" << endl;
 			for (int j=0; j<m-1; j++) {
-//				cout << "Dins del metode IIII" << endl;
 				exponent += (e[i+j+1]-48)*ele((double)10, m-j-2);
 			}
-//			cout << "Fora del metode IIII" << endl;
 			if (negative) {
 			numero = numero*-1;
 		}
@@ -107,35 +121,32 @@ void aproximacioSecant (int iteracions, int decimals) {
 	cout << "\nMetode de la secant" << endl;
 	cout << "-------------------" << endl;
 	cout << "Nombre de iteracions: " << iteracions << endl;
-	double X1 = (double)1;
-	double X2 = (double)-1;
+	double X1 = (double)0.1;
+	double X2 = (double)-0.1;
 	double Y1 = evaluar(X1);
 	double Y2 = evaluar(X2);
 	bool perfecte = false;
 	double puntTall;
-//	cout << "Valors de Y1 i Y2 " << Y1 << " / " << Y2 << endl;
 	cout.precision(decimals);
 	while (!((Y2 < 0 && Y1 > 0) || (Y2 > 0 && Y1 < 0))) {
-//		cout << "Acces" << endl;
-		X1 += 1;
-		X2 -= 1;
+		X1 += 0.1;
+		X2 -= 0.1;
 		Y1 = evaluar (X1);
 		Y2 = evaluar (X2);
+		cout << "no peta!!" << endl;
 	}
 	for(int i = 0; i<iteracions; i++) {
+		cout << "no peta" << i << endl;
 		Y1 = evaluar(X1);
 		Y2 = evaluar(X2);
 		double pendent = (Y2-Y1)/(X2-X1);
 		double ordenada = Y1-pendent*X1;
 		puntTall = (ordenada/pendent)*-1;
 		
-//		cout << "Valor compres entre " << fixed << X1 << " i " << X2 << endl;
-//		cout << "Valor aproximat: " << puntTall << endl;
-//		cout << "Evaluacio amb valor " << fixed << evaluar(puntTall) << endl;
 		if (evaluar (puntTall) > 0) {
 			X1 = puntTall;
 		}
-		if (evaluar (puntTall < 0)) {
+		if (evaluar (puntTall) < 0) {
 			X2 = puntTall;
 		}
 		if (evaluar(puntTall) == 0) {
@@ -148,6 +159,8 @@ void aproximacioSecant (int iteracions, int decimals) {
 		cout << "S. Evaluacio amb valor: " << fixed << puntTall << endl;
 		cout << "S. Hi ha present un error de: " << fixed << evaluar(puntTall) << endl;
 	}
+	cout << "no ha petat" << endl;
+	valSecant = evaluar(puntTall);
 }
 
 void aproximacioBolzano (int iteracions, int decimals) {
@@ -185,6 +198,7 @@ void aproximacioBolzano (int iteracions, int decimals) {
 		cout << "BW. Evaluacio amb valor: " << fixed << puntTall << endl;
 		cout << "BW. Hi ha present un error de: " << fixed << evaluar(puntTall) << endl;
 	}
+	valBolzano = evaluar(puntTall);
 }
 
 double evaluar (double x) {
@@ -193,4 +207,13 @@ double evaluar (double x) {
 		resultat += (double)coeficients[i]*(ele(x, exponents[i]));
 	}
 	return resultat;
+}
+
+void menu () {
+	cout << "Decisio: " << endl;
+	cout << "1. Aproximacio amb metode Bolzano-Weierstrass" << endl;
+	cout << "2. Aproximacio amb metode de la secant" << endl;
+	cout << "3. Comparacio entre els dos metodes" << endl;
+	cout << "0. Sortir" << endl;
+	cout << "Opcio: ";
 }
